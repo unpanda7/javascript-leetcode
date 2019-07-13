79f340b4a4215fb0148ef3244b2cffa3918f123a
unpanda7 <unpanda7@163.com>
2019-07-13 14:20:01 +0800

    添加一种新解法

--- 1-10/9.js
+++ 1-10/9.js
@@ -5,3 +5,13 @@
 var isPalindrome = function(x) {
     return parseInt(x.toString().split('').reverse().join('')) === x
 };
+
+// 第二种解法 栈
+var isPalindrome = function(x) {
+  const stack = []
+  let arr = x.toString().split('')
+  for(let i = arr.length - 1; i > 0; i--) {
+    stack.push(arr[i])
+  }
+  return parseInt(stack.join('')) === x
+};
