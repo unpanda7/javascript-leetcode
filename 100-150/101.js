f0a27df76550c1946c7a086faac6d682283b2f93
unpanda7 <unpanda7@163.com>
2020-06-28 19:46:22 +0800

    添加对称二叉树

---
+++ 100-150/101.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) return true
+    const isEqual = (left, right) => {
+      if (!left && !right) return true
+      if (!left || !right) return false
+      return left.val === right.val && isEqual(left.left, right.right) && isEqual(left.right, right.left)
+    }
+    return isEqual(root.left, root.right)
+};
