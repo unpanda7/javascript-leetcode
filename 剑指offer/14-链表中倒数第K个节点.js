fc3b5a7c4acd33de4eacb9f57642c9720f19de4d
unpanda7 <unpanda7@163.com>
2020-10-12 23:13:45 +0800

    add: offer 14

---
+++ 剑指offer/14-链表中倒数第K个节点.js
@@ -0,0 +1,26 @@
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function FindKthToTail(head, k)
+{
+    // write code here
+    if (head === null || k <= 0)  return null
+
+    let pNode1 = head,
+      pNode2 = head
+    while(--k) {
+      if (pNode1.next !== null) {
+        pNode2 = pNode2.next
+      } else {
+        return null
+      }
+    }
+
+    while(pNode2.next !== null) {
+      pNode1 = pNode1.next
+      pNode2 = pNode2.next
+    }
+
+    return pNode1
+}
