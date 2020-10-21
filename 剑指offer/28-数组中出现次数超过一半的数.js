3d374f3a9dea5a8c66996b07099c55136a07d22b
unpanda7 <unpanda7@163.com>
2020-10-21 23:05:49 +0800

    add: offer 28

---
+++ 剑指offer/28-数组中出现次数超过一半的数.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let count = 1;
+  let majority = nums[0];
+  for(let i = 1; i < nums.length; i++) {
+      if (count === 0) {
+          majority = nums[i];
+      }
+      if (nums[i] === majority) {
+          count ++;
+      } else {
+          count --;
+      }
+  }
+  return majority;
+}
